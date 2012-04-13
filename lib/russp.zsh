0fe35d54b565f853ed8a61db995c32022f488be4
Russ Posluszny <russ@russposluszny.com>
2012-04-13 10:18:25 -0400

    Removed repo edit plugin

--- lib/russp.zsh
+++ lib/russp.zsh
@@ -1,21 +1,4 @@
 
 function repo_edit() {
-
-  if [[ -n $(pwd | grep "svn/system/apps/puppet-vagrant/modules/") ]]; then
-      PKG="$(pwd | cut -d\/ -f 9)"
-  elif [[ -n $(pwd | grep "svn/system/apps/puppet/modules/") ]]; then
-      PKG="$(pwd | cut -d\/ -f 9)"
-  elif [[ -n $(pwd | grep "Checkouts/") ]]; then
-       PKG="$(pwd | cut -d\/ -f 5)"
-  elif [[ -n $(pwd | grep packages) ]]; then
-      PKG="$(pwd | cut -d\/ -f 6)"
-  elif [[ -n $(pwd | grep "svn/projects") ]]; then
-      PKG="$(pwd | cut -d\/ -f 7)"
-  elif [[ -n $(pwd | grep "russp/svn") ]]; then
-      PKG="$(echo $REPO | cut -d\/ -f 5)"
-  else
-      return
-  fi
-  echo "$REPO_PROMPT_PREFIX$PKG$REPO_PROMPT_SUFFIX"
-
+  # Disabled
 }
